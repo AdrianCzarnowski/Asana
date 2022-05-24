c41d404b070059b1d04f505ee699b2820f98252f
aczarnowski <adriannnczarnowski@gmail.com>
2022-05-24 15:00:43 +0200

    c

--- src/test/java/StepDefinitions/StepDefinitionsAsana.java
+++ src/test/java/StepDefinitions/StepDefinitionsAsana.java
@@ -4,6 +4,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static java.lang.System.getProperty;
+
 public class StepDefinitionsAsana {
     @Given("I have workspace object")
     public void i_have_workspace_object() {
@@ -18,5 +20,9 @@ public class StepDefinitionsAsana {
     @Then("User is able to see response with workspace details")
     public void user_is_able_to_see_response_with_workspace_details() {
         System.out.println("user_is_able_to_see_response_with_workspace_detailss");
+    }
+
+    public String getWeatherUrlAndEndpoint(){
+        return getProperty("BASE_URL");
     }
 }
