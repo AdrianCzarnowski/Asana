468e2fbd892ef2f2940ff053154d89624d80ba24
aczarnowski <adriannnczarnowski@gmail.com>
2022-06-14 13:47:04 +0200

    refactor

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -2,14 +2,7 @@ package baseTest;
 
 import helpers.Specification;
 import model.Project;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-
-import static io.restassured.RestAssured.given;
-import static java.lang.System.getProperty;
 
 
 public class BaseTest extends Specification {
